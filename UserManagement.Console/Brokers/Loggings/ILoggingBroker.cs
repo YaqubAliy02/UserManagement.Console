9303fbe37578c79a7a32980a71ac83f058684858
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-03-11 11:48:05 +0500

    Structure Project and Create CheckUserLogIn Method

---
+++ UserManagement.Console/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace UserManagement.Console.Brokers.Loggings
+{
+    internal interface ILoggingBroker
+    {
+        void LogError(string message);
+        void LogSuccessUser(string message);
+        void LogInformation(string message);
+        void LogError(Exception exception);
+    }
+}
---
+++ UserManagement.Console/Models/Credential.cs
@@ -0,0 +1,10 @@
+﻿
+namespace UserManagement.Console.Models
+{
+    internal class Credential
+    {
+        public string UserName { get; set; }
+        public int Password { get; set; }
+
+    }
+}
